ea8e3767d613b3eb5562c98e0f8c704d1605bf99
Cesarsp41 <64189944+Cesarsp41@users.noreply.github.com>
2022-05-11 00:40:05 -0500

    Update Practica2.py

--- Practica2.py
+++ Practica2.py
@@ -11,15 +11,33 @@ imagen2 = cv2.imread('im1.jpg',1)
 cv2.imshow("Batman",imagen1)
 cv2.moveWindow("Batman",0,200)
 cv2.imshow("Iron Man",imagen2)
-cv2.moveWindow("Iron Man",1975,200)
+cv2.moveWindow("Iron Man",1985,200)
 
 #Guardar tecla pulsada
 resultado = cv2.waitKey(0)
 
 
+#METODO1 CV2 FUNCIONES
+
 if resultado == 99:
-    imagen3 = cv2.add(imagen1,imagen2)
-    cv2.imshow("Suma",imagen3)
+    suma = cv2.add(imagen1,imagen2) #Variable imagen de suma
+    cv2.imshow("Suma",suma)
     cv2.moveWindow("Suma",900,200)
 
 
+if resultado == 100:
+    resta = cv2.subtract(imagen1,imagen2) #Variable imagen de resta
+    cv2.imshow("Resta",resta)
+    cv2.moveWindow("Resta",900,200)
+
+
+if resultado == 101:
+    multiplicacion = cv2.multiply(imagen1,imagen2) #Variable imagen de multiplicacion
+    cv2.imshow("Multiplicacion",multiplicacion)
+    cv2.moveWindow("Multiplicacion",900,200)
+
+
+if resultado == 102:
+    division = cv2.divide(imagen1,imagen2) #Variable imagen de division
+    cv2.imshow("Division",division)
+    cv2.moveWindow("Division",900,200)
